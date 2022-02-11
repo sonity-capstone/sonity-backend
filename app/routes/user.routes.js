e6a96c3ef7c48fc041964058ccd01cc01e773bd4
Simon Bermudez <sabdz55@gmail.com>
2022-02-11 14:27:44 -0500

    Gary and Duncan changes - AUTH

---
+++ app/routes/user.routes.js
@@ -0,0 +1,28 @@
+const { authJWT } = require("../middleware");
+const controller = require("../controllers/user.controller");
+
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  app.get("/test/all", controller.allAccess);
+
+  app.get(
+    "/test/user",
+    [authJWT.verifyToken],
+    controller.userBoard
+  );
+
+  
+
+  app.get(
+    "/test/admin",
+    [authJWT.verifyToken, authJWT.isAdmin],
+    controller.adminBoard
+  );
+};
