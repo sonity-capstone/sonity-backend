e9160665e0cce9f61fdfa56a91c59b5a6765299d
Simon Bermudez <sabdz55@gmail.com>
2022-03-21 19:51:23 -0400

    added new routes

--- app/routes/leads.routes.js
+++ app/routes/leads.routes.js
@@ -8,13 +8,105 @@ const express = require('express');
 const router = express();
 
 router.get("/", (req, res) => {
+    
+    location_country_id = req.query.c_id ? req.query.c_id : 235
+    location_region_id  = req.query.r_id ? req.query.r_id : 34
+    location_metro_id   = req.query.m_id ? req.query.m_id : 285
+    industry_id         = req.query.i_id ? req.query.i_id : 20
     leadsDB.query(`
-        SELECT * FROM li_data.get_profiles_list(235,34,285,20)
+        SELECT * FROM li_data.get_profiles_list(
+            ${location_country_id},
+            ${location_region_id},
+            ${location_metro_id},
+            ${industry_id}
+        )
     `, (err, leads) => {
         if (err) {
             res.send({error: err.toString()})
         } else {
             res.send(leads.rows)
+        }
+    })
+});
+
+router.get("/countries", (req, res) => {
+    leadsDB.query(`
+        SELECT * FROM reference.countries
+    `, (err, countries) => {
+        if (err) {
+            res.send({error: err.toString()})
+        } else {
+            // res.send(countries.rows)
+            res.send([
+                {
+                    "id": 39,
+                    "linkedin_label": "Canada",
+                    "alternate": "Canada",
+                    "code_iso_2": "CA",
+                    "code_iso_3": "CAN",
+                    "code_numeric": "124",
+                    "code_iso_3166_2": "ISO 3166-2:CA",
+                    "un_region": "Americas",
+                    "un_sub_region": "Northern America",
+                    "un_intermediate_region": "",
+                    "un_region_code": "019",
+                    "un_sub_region_code": "021",
+                    "un_intermediate_region_code": "",
+                    "disolved": null
+                },
+                {
+                    "id": 235,
+                    "linkedin_label": "United States",
+                    "alternate": "United States of America",
+                    "code_iso_2": "US",
+                    "code_iso_3": "USA",
+                    "code_numeric": "840",
+                    "code_iso_3166_2": "ISO 3166-2:US",
+                    "un_region": "Americas",
+                    "un_sub_region": "Northern America",
+                    "un_intermediate_region": "",
+                    "un_region_code": "019",
+                    "un_sub_region_code": "021",
+                    "un_intermediate_region_code": "",
+                    "disolved": null
+                }
+            ])
+        }
+    })
+});
+
+router.get("/regions/:country_id", (req, res) => {
+    leadsDB.query(`
+        SELECT * FROM reference.na_regions WHERE country_id = ${req.params.country_id}
+    `, (err, countries) => {
+        if (err) {
+            res.send({error: err.toString()})
+        } else {
+            res.send(countries.rows)
+        }
+    })
+});
+
+router.get("/metro_areas/:region_id", (req, res) => {
+    leadsDB.query(`
+        SELECT * FROM reference.na_metro_areas WHERE region_id = ${req.params.region_id}
+    `, (err, countries) => {
+        if (err) {
+            res.send({error: err.toString()})
+        } else {
+            res.send(countries.rows)
+        }
+    })
+});
+
+router.get("/industries", (req, res) => {
+    leadsDB.query(`
+        SELECT * FROM reference.industries
+    `, (err, countries) => {
+        if (err) {
+            res.send({error: err.toString()})
+        } else {
+            res.send(countries.rows)
         }
     })
 });
